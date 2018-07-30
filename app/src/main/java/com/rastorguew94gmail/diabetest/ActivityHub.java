eb9a2acfdef864c04250289d8adfd40540758f9e
Расторгуев Илья Анатольевич <rastorguew94@gmail.com>
2018-07-30 14:40:46 +0300

    init commit

---
+++ app/src/main/java/com/rastorguew94gmail/diabetest/ActivityHub.java
@@ -0,0 +1,14 @@
+package com.rastorguew94gmail.diabetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub);
+    }
+}
